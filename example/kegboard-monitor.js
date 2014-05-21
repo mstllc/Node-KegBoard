30baa1fa09d2a38c489c519b33c7db232233895a
Max Thorson <max.thorson@collemcvoy.com>
2014-05-21 13:50:30 -0500

    v 0.0.2, functioning serial listener module

--- example/kegboard-monitor.js
+++ example/kegboard-monitor.js
@@ -1,1 +1,24 @@
+// Require KegBoard module
 var kegboard = require('../lib/kegboard.js');
+
+// Check for connected KegBoard
+console.log('Looking for KegBoard...');
+kegboard.checkForKegboard(function(err, board) {
+  if(err) {
+    console.log('Error finding KegBoard', err);
+    return false;
+  } else {
+    console.log('Found: %s', board.toString());
+    console.log('Listening to KegBoard...');
+
+    // Board 'open' method must be called to send or receive messages
+    board.open(function() {
+
+      // Listen for messages
+      board.on('message', function(message) {
+        console.log(message.toString());
+      });
+
+    });
+  }
+});
